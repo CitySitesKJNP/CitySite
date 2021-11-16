1c15cf1de8762f6e94adf856b8d88ddb974ece07
Jonathan <jonathan.trhodes07@gmail.com>
2021-11-16 09:47:47 -0600

    starting stuff

---
+++ src/main/java/com/example/citysites/CitySitesApplication.java
@@ -0,0 +1,13 @@
+package com.example.citysites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitySitesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitySitesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/citysites/CitySitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.citysites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitySitesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
