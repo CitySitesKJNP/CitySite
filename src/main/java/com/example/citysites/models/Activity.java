8707028e7f0aed6a227b60217fe8405794657c31
Kyle Schrader <kyle.schrader97@gmail.com>
2021-11-16 12:23:34 -0600

    created first draft of models

---
+++ src/main/java/com/example/citysites/models/Activity.java
@@ -0,0 +1,9 @@
+package com.example.citysites.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "activities")
+public class Activity {
+}
---
+++ src/main/java/com/example/citysites/models/ReviewImage.java
@@ -0,0 +1,9 @@
+package com.example.citysites.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "review_images")
+public class ReviewImage {
+}
