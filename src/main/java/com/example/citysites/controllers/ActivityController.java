c971a79dd028a57658372486eb86eac51f2430ab
Kyle Schrader <kyle.schrader97@gmail.com>
2021-11-19 15:55:58 -0600

    can create activities through /activity/create. entries are saved to DB

--- src/main/java/com/example/citysites/controllers/ActivityController.java
+++ src/main/java/com/example/citysites/controllers/ActivityController.java
@@ -1,12 +1,31 @@
 package com.example.citysites.controllers;
 
 import com.example.citysites.models.Activity;
+import com.example.citysites.models.ActivityImage;
+import com.example.citysites.models.User;
+import com.example.citysites.repositories.ActivityRepository;
+import com.example.citysites.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ActivityController {
+    private UserRepository userDao;
+    private ActivityRepository activityDao;
+
+    public ActivityController(UserRepository userDao, ActivityRepository activityDao) {
+        this.userDao = userDao;
+        this.activityDao = activityDao;
+    }
+
     @GetMapping("/activity/details")
     public String singleActivityDetails() {
         return "citysites/details";
@@ -16,6 +35,22 @@ public class ActivityController {
     public String activityCreationPage(Model model) {
         model.addAttribute("activity", new Activity());
         return "citysites/add-activity";
+    }
+
+    @PostMapping("/activity/create")
+    public String createActivity(@ModelAttribute Activity activity, @RequestParam List<String> urls) {
+        List<ActivityImage> images = new ArrayList<>();
+
+        for (String url : urls) {
+            ActivityImage activityImage = new ActivityImage(url);
+            activityImage.setActivity(activity);
+            images.add(activityImage);
+        }
+
+        activity.setActivityImages(images);
+
+        activityDao.save(activity);
+        return "redirect:/home";
     }
 
 //    PostMapping to add new Activity to DB
