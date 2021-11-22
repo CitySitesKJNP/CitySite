a84a0d6120cbe8480919e9caf85c21d76300201e
Kyle Schrader <kyle.schrader97@gmail.com>
2021-11-22 11:50:28 -0600

    finished review creation form and functionality. will need to fill in the logic for activity_id when we have activities configured

--- src/main/java/com/example/citysites/controllers/ReviewController.java
+++ src/main/java/com/example/citysites/controllers/ReviewController.java
@@ -1,12 +1,23 @@
 package com.example.citysites.controllers;
 
 import com.example.citysites.models.Review;
+import com.example.citysites.models.User;
+import com.example.citysites.repositories.ReviewRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ReviewController {
+    ReviewRepository reviewDao;
+
+    public ReviewController(ReviewRepository reviewDao) {
+        this.reviewDao = reviewDao;
+    }
+
     @GetMapping("/reviews")
     public String reviewsPage() {
         return "citysites/view-reviews";
@@ -18,5 +29,11 @@ public class ReviewController {
         return "citysites/add-review";
     }
 
-//    PostMapping for Review Creation
+    @PostMapping("/reviews/create")
+    public String createReview(@ModelAttribute Review review) {
+        User principal = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        review.setUserReview(principal);
+        reviewDao.save(review);
+        return "redirect:/reviews";
+    }
 }
