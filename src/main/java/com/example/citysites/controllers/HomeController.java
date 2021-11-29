514dda993f6dba2829f2424948dab22f0384df82
Kyle Schrader <kyle.schrader97@gmail.com>
2021-11-29 17:04:40 -0600

    making progress on User Created Activities showing up on main map

--- src/main/java/com/example/citysites/controllers/HomeController.java
+++ src/main/java/com/example/citysites/controllers/HomeController.java
@@ -1,12 +1,21 @@
 package com.example.citysites.controllers;
 
+import com.example.citysites.repositories.ActivityRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    ActivityRepository activityDao;
+
+    public HomeController(ActivityRepository activityDao) {
+        this.activityDao = activityDao;
+    }
+
     @GetMapping("/home")
-    public String homePage() {
+    public String homePage(Model model) {
+        model.addAttribute("activities", activityDao.findAll());
         return "citysites/home";
     }
 }
