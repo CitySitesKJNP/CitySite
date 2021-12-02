1ad4d59dd6f5dc3183a07496dee6ba15e1293b3e
Kyle Schrader <kyle.schrader97@gmail.com>
2021-12-02 15:16:23 -0600

    reverse geocoded lng/lat on details page

--- src/main/java/com/example/citysites/models/Activity.java
+++ src/main/java/com/example/citysites/models/Activity.java
@@ -1,12 +1,14 @@
 package com.example.citysites.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Table(name = "activities")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Activity {
 
     @Id
