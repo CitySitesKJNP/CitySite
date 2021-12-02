6a29b5c8c7eae0ba3b29053240673d31a2cea8af
Kyle Schrader <kyle.schrader97@gmail.com>
2021-12-02 16:36:51 -0600

    favorites add and favorites view functionality

--- src/main/java/com/example/citysites/controllers/UserController.java
+++ src/main/java/com/example/citysites/controllers/UserController.java
@@ -68,22 +68,33 @@ public class UserController {
     @GetMapping("/user/favorites")
     public String userFavoritesPage(Model model, Principal principal) {
         User loggedInUser = userDao.findByUsername(principal.getName());
+        List<Activity> activityList = loggedInUser.getFavoriteActivities();
         model.addAttribute("user", loggedInUser);
+        model.addAttribute("favorites", activityList);
         return "citysites/favorites";
     }
 
     @PostMapping("/activity/{id}/favorite")
-    public String userFavorites(Principal principal) {
+    public String userFavorites(@PathVariable long id, Principal principal) {
+//        logged in user object
         User loggedInUser = userDao.findByUsername(principal.getName());
-        Activity favorited = 
+
 //        need to pull out the id from the pathvariable
 //        with that path variable retrieve that specific activity from the database by id
+        Activity activity = activityDao.getById(id);
+
 //        from the logged in userobject pull out the list of favorite activities and store it in a list of activities variable
+        List<Activity> activityList = loggedInUser.getFavoriteActivities();
+
 //        add the activity object from the path variable to the list of favorites activities from the user
+        activityList.add(activity);
+
 //        then youll need to set the value of the list of favorite activities on the user object that you added an activity to
+        loggedInUser.setFavoriteActivities(activityList);
+
 //        persist the change by saving the user record with the user dao
-//
-        return "redirect:/home";
+        userDao.save(loggedInUser);
+        return "redirect:/user/favorites";
     }
 
 }
